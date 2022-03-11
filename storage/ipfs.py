eac5bda0e7f7916e106b173d7dcaae45070a114b
vintagegold <vintagegold123@gmail.com>
2022-03-11 13:20:56 -0500

    support public gateway

--- storage/ipfs.py
+++ storage/ipfs.py
@@ -1,20 +1,35 @@
+from io import StringIO
+import pandas as pd
+
+def read_file(b):
+
+    s=str(b,'utf-8')
+
+    data = StringIO(s) 
+
+    df=pd.read_csv(data)
+    
+    return df
+
 #https://docs.ipfs.io/reference/http/api/#getting-started
 import requests
+import random
+
 class IPFS:
 
-    def get_peers():
+    def get_peers(self):
 
         return requests.post("http://127.0.0.1:5001/api/v0/swarm/peers",).json()
 
     #Flags aren't working
-    def add(fn):
+    def add(self,fn):
 
         params = {'progress':True,
                  "silent":False
                  }
 
         files = {
-        'file': ('myfile', open(fn, 'rb')),
+        'file': (fn, open(fn, 'rb')),
         }
 
         response = requests.post('http://127.0.0.1:5001/api/v0/add', json=params, files=files)
@@ -23,11 +38,26 @@ class IPFS:
 
         return response
 
-    def get_file(key):
+    def get_file(self,key,local_node=True):
         params = (('arg', key),)
+        
+        if local_node:
 
-        response = requests.post('http://127.0.0.1:5001/api/v0/block/get', params=params)
+            response = requests.post('http://127.0.0.1:5001/api/v0/cat?', params=params)
+            
+        else:
+            
+            
+            #Sourced - All had green checkmarks as of 03/08/2022
+            #https://ipfs.github.io/public-gateway-checker/
+            gateways = ["https://infura-ipfs.io","https://cf-ipfs.com","https://dweb.link","https://astyanax.io"]
+            
+            selected_gateway = gateways[random.randint(0,len(gateways)-1)]
+            
+            
+            response = requests.get(f"{selected_gateway}/ipfs/{key}")
+            
 
-        print("Retrieved file hash",key,"from IPFS -","Response",response.status_code)
+        print("Retrieved file hash",key,f"from {selected_gateway}","Response",response.status_code)
 
         return response
